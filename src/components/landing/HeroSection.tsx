9f867caf7425c368218bbf651b801310180f55a4
Ilhan Zuldan <ilhanzuldan11@gmail.com>
2025-07-15 04:49:09 +0000

    still no images at all, fix it, i think its so simple and you cant do th

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -53,7 +53,7 @@ export function HeroSection() {
                 />
             </div>
         </div>
-         <div className="absolute inset-0 bg-gradient-to-br from-background via-accent/40 to-background z-0"></div>
+         <div className="absolute inset-0 bg-gradient-to-br from-background via-accent/20 to-background z-0"></div>
     </section>
   );
 }
