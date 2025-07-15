c3b38c1afc5d89716bc0966922f4c653c2feeb2a
Ilhan Zuldan <ilhanzuldan11@gmail.com>
2025-07-15 04:37:14 +0000

    change the font become better and playful

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Nunito', 'sans-serif'],
+        headline: ['"Baloo 2"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
